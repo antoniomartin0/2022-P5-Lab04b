6da7035040a02e8e20c2fdb7c1ab9af69de3c9f8
Lab03 <antonio.martin@vtsd.com>
2022-10-19 18:52:22 -0400

    Lab04b

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -14,15 +14,15 @@ public class Lab04bvst extends Applet
     {
         // DRAW CUBE
 
-        g.drawLine(25,-25,75,-75);
-        g.drawLine(25,-25,25,-150);
-        g.drawLine(25,-25,150,-25);
-        g.drawLine(25,-150,75,-200);
-        g.drawLine(150,-25,200,-75);
-        g.drawLine(75,-75,200,-75);
-        g.drawLine(75,-75,75,-200);
-        g.drawLine(200,-75,200,-200);
-        g.drawLine(75,-200,200,-200);
+        g.drawLine(25,25,75,75);
+        g.drawLine(25,25,25,150);
+        g.drawLine(25,25,150,25);
+        g.drawLine(25,150,75,200);
+        g.drawLine(150,25,200,75);
+        g.drawLine(75,75,200,75);
+        g.drawLine(75,75,75,200);
+        g.drawLine(200,75,200,200);
+        g.drawLine(75,200,200,200);
 
         // DRAW SPHERE
 
@@ -33,47 +33,50 @@ public class Lab04bvst extends Applet
         g.drawLine(650,-175,750,-275);
         g.drawLine(750,-275,850,-100);
 
+        g.drawOval(700,250,75,125);
+        g.drawOval(650,100,200,225);
+
         // DRAW APCS
 
-        g.drawRect(25,-475,50,-600);
-        g.drawRect(50,-475,100,-500);
-        g.drawRect(50,-525,100,-550);
-        g.drawRect(75,-475,100,-600);
-        g.drawRect(50,-500,75,-525);
-        g.fillRect(25,-475,50,-600);
-        g.fillRect(50,-475,100,-500);
-        g.fillRect(50,-525,100,-550);
-        g.fillRect(75,-475,100,-600);
-        g.fillRect(50,-500,75,-525);
+        g.drawRect(25,475,25,125);
+        g.drawRect(50,475,50,25);
+        g.drawRect(75,500,25,100);
+        g.drawRect(50,500,25,25);
+        g.drawRect(50,525,25,25);
+        g.fillRect(25,475,25,125);
+        g.fillRect(50,475,50,25);
+        g.fillRect(75,500,25,100);
+        g.fillRect(50,500,25,25);
+        g.fillRect(50,525,25,25);
 
-        g.drawRect(125,-475,150,-600);
-        g.drawRect(150,-475,200,-500);
-        g.drawRect(175,-500,200,-525);
-        g.drawRect(150,-500,175,-525);
-        g.drawRect(150,-525,200,-550);
-        g.fillRect(125,-475,150,-600);
-        g.fillRect(150,-475,200,-500);
-        g.fillRect(175,-500,200,-525);
-        g.fillRect(150,-500,175,-525);
-        g.fillRect(150,-525,200,-550);
+        g.drawRect(125,475,25,125);
+        g.drawRect(150,475,50,25);
+        g.drawRect(150,500,25,25);
+        g.drawRect(150,525,25,25);
+        g.drawRect(175,500,25,50);
+        g.fillRect(125,475,25,125);
+        g.fillRect(150,475,50,25);
+        g.fillRect(150,500,25,25);
+        g.fillRect(150,525,25,25);
+        g.fillRect(175,500,25,50);
 
-        g.drawRect(225,-475,300,-500);
-        g.drawRect(225,-500,250,-600);
-        g.drawRect(250,-575,300,-600);
-        g.fillRect(225,-475,300,-500);
-        g.fillRect(225,-500,250,-600);
-        g.fillRect(250,-575,300,-600);
+        g.drawRect(225,475,25,125);
+        g.drawRect(250,475,50,25);
+        g.drawRect(250,575,50,25);
+        g.fillRect(225,475,25,125);
+        g.fillRect(250,475,50,25);
+        g.fillRect(250,575,50,25);
 
-        g.drawRect(325,-475,400,-500);
-        g.drawRect(325,-500,350,-550);
-        g.drawRect(350,-525,400,-550);
-        g.drawRect(375,-550,400,-600);
-        g.drawRect(325,-575,375,-600);
-        g.fillRect(325,-475,400,-500);
-        g.fillRect(325,-500,350,-550);
-        g.fillRect(350,-525,400,-550);
-        g.fillRect(375,-550,400,-600);
-        g.fillRect(325,-575,375,-600);
+        g.drawRect(325,475,75,25);
+        g.drawRect(325,500,25,50);
+        g.drawRect(350,525,50,25);
+        g.drawRect(375,550,25,50);
+        g.drawRect(325,575,75,25);
+        g.fillRect(325,475,75,25);
+        g.fillRect(325,500,25,50);
+        g.fillRect(350,525,50,25);
+        g.fillRect(375,550,25,50);
+        g.fillRect(325,575,75,25);
 
 
         // DRAW PACMEN FLOWER
