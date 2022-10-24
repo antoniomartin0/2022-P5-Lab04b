0cade8f92e26d6265f37d39815b26a4f0e301fd8
Lab03 <antonio.martin@vtsd.com>
2022-10-24 19:02:11 -0400

    Temp

---
+++ temp.java
@@ -0,0 +1,8 @@
+public class temp {
+}
+    // Write a loop that outputs the numbers from 100 to 1 by 3's
+    // Ex: 100, 97, 94, 91, 88, ...
+    for(int k=0; k <= 100; k+=3);
+    System.out.print(k + ", ");
+    for (int k = 1; k <= 100; k++);
+    System.out.print(p + " ");
